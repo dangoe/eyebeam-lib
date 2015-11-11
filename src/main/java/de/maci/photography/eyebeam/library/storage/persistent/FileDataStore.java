b14bdb4f15dfe48ffe9ee9eb83753789b673082d
Daniel Götten <daniel.goetten@gmail.com>
2015-11-11 20:58:48 +0100

    License corrected

--- src/main/java/de/maci/photography/eyebeam/library/storage/persistent/FileDataStore.java
+++ src/main/java/de/maci/photography/eyebeam/library/storage/persistent/FileDataStore.java
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2015 Daniel Götten
  * <p/>
- * Permission is hereby granted, free fromMetadata charge, to any person obtaining a copy
- * fromMetadata this software and associated documentation files (the "Software"), to deal
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * fromMetadata the Software, and to permit persons to whom the Software is furnished to
+ * of the Software, and to permit persons to whom the Software is furnished to
  * do so, subject to the following conditions:
  * <p/>
  * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions fromMetadata the Software.
+ * copies or substantial portions of the Software.
  * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
